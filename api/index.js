ee168e1c73c185b67af4d78baecfb373e8a3bdc8
andrtvrd <andreytwerd@gmail.com>
2023-10-10 11:20:06 +0300

    updated code

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
 //Import the necessary libraries/declare the necessary objects
 var app = require("express")()
 var myParser = require("body-parser");
-var request = require("request")
+var req = require("request")
 var dataFromCeligo;
 var flowId = "";
 
+const bearerToken = "716451a806e4454abfba9fcf8959c324"
+let celigoResponse
+let jobId
+
 app.use(myParser.json());
-app.get("/", function(request, response) {
+app.get('/', function(request, response) {
     response.setHeader('Content-Type', 'text/html');
     response.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     response.send("Hello this is Home")
@@ -19,18 +23,30 @@ app.post("/api/test/source-data", function(request, response) {
 });
 //get id of record created in system 1 and run a flow under Test
 // flow id for tests - 6511e24f876889750ba5f674
-app.post(`/flows/${flowId}/run`, function(request, response) {
+app.post('/api/flows/6511e24f876889750ba5f674/run', function(request, response) {
     idOfRecordCreated = request.body
-    console.log(flowId)
-    let celigoResponse = request.post({
-        url: `https://api.integrator.io/v1/flows/${flowId}/run`,
+    console.log(idOfRecordCreated)
+    req({
+        method: 'POST',
+        uri: 'https://api.integrator.io/v1/flows/6511e24f876889750ba5f674/run',
         json: true,
         auth: {
-            bearer: '716451a806e4454abfba9fcf8959c324'
+            bearer: bearerToken
           }
-    })
-    console.log(celigoResponse)
+    },
+        function (error, resp, body) {
+            celigoResponse = resp
+            jobId = body
+            console.log(jobId)
+            response.send(jobId)
+        }
+    )
+});
 
+app.get('/test_delay', function(request, response) {
+    setTimeout((() => {
+        response.send('Hello this is Home');
+      }), 4000)
 });
 
 //module.exports = app;
